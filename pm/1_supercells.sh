4ee29a2c7628534593ee31593ad217868bd0191c
pablogila <pablo.gila.herranz@gmail.com>
2024-12-06 00:04:51 +0100

    comments

--- pm/1_supercells.sh
+++ pm/1_supercells.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
+# Default displacements by phonopy are 0.01 Angstrom
 phonopy --qe -d --dim="2 2 2" -c scf.in
-#phonopy --qe -d --dim="2 2 2" -c scf.in --tolerance=1e-3
--- pm/4_forces.sh
+++ pm/4_forces.sh
@@ -2,7 +2,6 @@
 
 # creates FORCE_SETS file from QE output of supercell calculations
 phonopy -f supercell-*.out
-#phonopy --tolerance 1e-3 --include-all -f supercell-*.out
 
 # Reads the new FORCE_SETS file and creates a phonopy.yaml file
 phonopy --include-all
